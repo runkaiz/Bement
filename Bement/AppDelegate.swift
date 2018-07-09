55e83754ba0529c2ff33b148ee5e86080cccb400
Zirui Chen <bambimao@200525@gmail.com>
2018-07-09 18:58:46 +0800

    New Launch screen

--- Bement/AppDelegate.swift
+++ Bement/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+   
         // Override point for customization after application launch.
-        return true
+    
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -42,5 +42,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
-}
+
 
